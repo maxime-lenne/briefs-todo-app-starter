63e11a8f9570630816e9ae24070b8cf34e1a4f1c
fatima <ouaayi.fatima91@gmail.com>
2026-06-24 16:16:44 +0200

    Fix package.json and commitlint config for npm compatibility

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
-  extends: ['@commitlint/config-gitmoji'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-case': [0],
     'subject-empty': [2, 'never'],
@@ -17,57 +17,46 @@ module.exports = {
           feat: {
             description: 'A new feature',
             title: 'Features',
-            emoji: '✨',
           },
           fix: {
             description: 'A bug fix',
             title: 'Bug Fixes',
-            emoji: '🐛',
           },
           docs: {
             description: 'Documentation only changes',
             title: 'Documentation',
-            emoji: '📚',
           },
           style: {
             description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
             title: 'Styles',
-            emoji: '🎨',
           },
           refactor: {
             description: 'A code change that neither fixes a bug nor adds a feature',
             title: 'Code Refactoring',
-            emoji: '🏗️',
           },
           perf: {
             description: 'A code change that improves performance',
             title: 'Performance Improvements',
-            emoji: '⚡️',
           },
           test: {
             description: 'Adding missing tests',
             title: 'Tests',
-            emoji: '🧪',
           },
           build: {
             description: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
             title: 'Builds',
-            emoji: '🔨',
           },
           ci: {
             description: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
             title: 'Continuous Integrations',
-            emoji: '🚜',
           },
           chore: {
             description: "Other changes that don't modify src or test files",
             title: 'Chores',
-            emoji: '🧹',
           },
           revert: {
             description: 'Reverts a previous commit',
             title: 'Reverts',
-            emoji: '🔄',
           },
         },
       },
