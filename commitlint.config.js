d86ccc42780f494196f36114ec6531eea1182c3a
fatima <ouaayi.fatima91@gmail.com>
2026-06-24 16:00:12 +0200

    Add commitlint and gitmoji for commit message conventions

---
+++ commitlint.config.js
@@ -0,0 +1,106 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-gitmoji'],
+  rules: {
+    'subject-case': [0],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 100],
+  },
+  prompt: {
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing",
+        enum: {
+          feat: {
+            description: 'A new feature',
+            title: 'Features',
+            emoji: '✨',
+          },
+          fix: {
+            description: 'A bug fix',
+            title: 'Bug Fixes',
+            emoji: '🐛',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📚',
+          },
+          style: {
+            description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+            title: 'Styles',
+            emoji: '🎨',
+          },
+          refactor: {
+            description: 'A code change that neither fixes a bug nor adds a feature',
+            title: 'Code Refactoring',
+            emoji: '🏗️',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            title: 'Performance Improvements',
+            emoji: '⚡️',
+          },
+          test: {
+            description: 'Adding missing tests',
+            title: 'Tests',
+            emoji: '🧪',
+          },
+          build: {
+            description: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+            title: 'Builds',
+            emoji: '🔨',
+          },
+          ci: {
+            description: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+            title: 'Continuous Integrations',
+            emoji: '🚜',
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: 'Chores',
+            emoji: '🧹',
+          },
+          revert: {
+            description: 'Reverts a previous commit',
+            title: 'Reverts',
+            emoji: '🔄',
+          },
+        },
+      },
+      scope: {
+        description: 'What is the scope of this change (e.g. component or file name)',
+      },
+      subject: {
+        description: 'Write a SHORT, IMPERATIVE tense description of the change',
+      },
+      body: {
+        description: 'Provide a LONGER description of the change (optional). Use "|" to break new line',
+      },
+      footer: {
+        description: 'Cite any breaking changes or deprecations (optional)',
+      },
+      isBreaking: {
+        description: 'Are there any breaking changes?',
+      },
+      breakingBody: {
+        description: 'A BREAKING CHANGE commit requires a body. Please enter the longer description of the change above',
+      },
+      breaking: {
+        description: 'DESCRIBE the breaking changes',
+      },
+      isIssueAffected: {
+        description: 'Does this change affect any open issues?',
+      },
+      issuesBody: {
+        description: 'Add issue references (e.g. "fix #123", "re #123"). Add each issue number in a new line',
+      },
+      issues: {
+        description: 'Add issue references (e.g. "fix #123", "re #123")',
+      },
+    },
+  },
+};
